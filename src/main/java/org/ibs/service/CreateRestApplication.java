f9f2a4fcf3f73ded9281d3b2889813e5d840d229
MaxonSQL <maxom7991@gmail.com>
2022-11-29 18:12:56 +0400

    Начало

---
+++ src/main/java/org/ibs/service/CreateRestApplication.java
@@ -0,0 +1,13 @@
+package org.ibs.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ibs/service/CreateRestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ibs.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
