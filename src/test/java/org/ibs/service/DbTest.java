6aade4b366024436b6c3e3013c7a3949853ea851
MaxonSQL <maxom7991@gmail.com>
2022-11-30 01:21:28 +0400

    DbTest First Test

--- src/test/java/org/ibs/service/DbTest.java
+++ src/test/java/org/ibs/service/DbTest.java
@@ -7,6 +7,7 @@ import jakarta.persistence.PersistenceContext;
 import org.ibs.service.domain.Course;
 import org.ibs.service.domain.Department;
 import org.ibs.service.domain.Employee;
+import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +17,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 
 @RunWith(SpringRunner.class)
@@ -33,20 +35,26 @@ class DbTest {
     void setup() {
         Department department = new Department(null, "IT");
         Course course = new Course(null, "Rest service");
-        //   Employee emp = new Employee(null, "a", "b", LocalDate.now(), );
+        Employee emp = new Employee(null, "a", "b",
+                LocalDate.now(), department, 500000, List.of(course));
 
         em.persist(department);
         em.persist(course);
+        em.persist(emp);
     }
 
     @Test
-    void test(){
+    void test() {
+        Assert.assertEquals(1, em.createQuery("FROM Department").getResultList().size());
+
+        Employee emp = em.createQuery("FROM Employee", Employee.class)
+                .setMaxResults(1)
+                .getResultList()
+                .get(0);
+
+        Assert.assertEquals("Rest service", emp.getCourses().get(0).getName());
 
     }
-
-
-
-
 }
 
 
