ae89e5031bd204770352fa455ed61331508b08c4
MaxonSQL <maxom7991@gmail.com>
2022-11-30 00:35:12 +0400

    DbTest 2/3

--- src/test/java/org/ibs/service/DbTest.java
+++ src/test/java/org/ibs/service/DbTest.java
@@ -1,11 +1,12 @@
 package org.ibs.service;
 
 
-import org.ibs.service.domain.Employee;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.ibs.service.domain.Course;
 import org.ibs.service.domain.Department;
+import org.ibs.service.domain.Employee;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -13,6 +14,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 
 @RunWith(SpringRunner.class)
@@ -27,10 +30,10 @@ class DbTest {
 
 
     @BeforeEach
-
-        void setup() {
+    void setup() {
         Department department = new Department(null, "IT");
         Course course = new Course(null, "Rest service");
+        //   Employee emp = new Employee(null, "a", "b", LocalDate.now(), );
 
         em.persist(department);
         em.persist(course);
