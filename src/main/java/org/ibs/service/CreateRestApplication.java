eb3e2f97513d0bcae9dcc868a2caafc515d2c3ac
MaxonSQL <maxom7991@gmail.com>
2022-12-03 12:42:15 +0400

    Add rep interface java + test2

--- src/main/java/org/ibs/service/CreateRestApplication.java
+++ src/main/java/org/ibs/service/CreateRestApplication.java
@@ -3,13 +3,22 @@ package org.ibs.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@RestController
 public class CreateRestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreateRestApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String index() {
+		return "Hello main";
+
 	}
 
 }
---
+++ src/main/java/org/ibs/service/bussines/GreetingServiceImpl.java
@@ -0,0 +1,14 @@
+package org.ibs.service.bussines;
+
+import org.ibs.service.domain.entity.Employee;
+
+public class GreetingServiceImpl {
+
+    void sayHello(Employee employee) {
+
+
+
+
+    }
+
+}
